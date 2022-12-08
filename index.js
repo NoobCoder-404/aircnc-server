c729937fa570e32493f67023e282a3ea6656f0bc
Noob-H4cker <alsamiul123@gmail.com>
2022-12-08 12:03:33 +0600

    jwt token

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+/* eslint-disable no-empty */
+/* eslint-disable no-unused-vars */
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const Port = process.env.Port || 5000;
@@ -8,10 +12,44 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.rqwof3p.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+    try {
+        const userCollection = client.db('aircnc').collection('users');
+
+        app.put('/user/:email', async (req, res) => {
+            const { email } = req.params;
+            const user = req.body;
+            const filter = { email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user,
+            };
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+            console.log(result);
+
+            const token = jwt.sign(user, process.env.ACCESS_SECRET_TOKEN, { expiresIn: '1d' });
+            console.log(token);
+            res.send({ result, token });
+
+            console.log('Database Connected');
+        });
+    } finally {
+    }
+}
+run().catch((error) => console.error(error.message));
+
 app.get('/', (req, res) => {
-    res.send('Server Running');
+    res.send('Aircnc Server Running');
 });
 
 app.listen(Port, () => {
-    console.log(`Server Running on Port ${Port}`);
+    console.log(`Aircnc Server Running on Port ${Port}`);
 });
